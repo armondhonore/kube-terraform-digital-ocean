f00035d85aa097bf637420a3776e3c8e6679339d
armondhonore <armond.honore@gmail.com>
2017-08-14 09:40:54 -0400

    add some changes to remove hardcoaded values

--- terraform/master/providers.tf
+++ terraform/master/providers.tf
@@ -1,7 +1,7 @@
 # Set the variable value in *.tfvars file
 # or using -var="do_token=..." CLI option
 variable "do_token" {
-default ="6897972000dd8987801eabf70e599b974049dc78d5c0b31d620b67dccf461f85"
+default ="TO_DEBUG_ADD_DO_TOKEN_HERE"
 }
 
 # Configure the DigitalOcean Provider
--- terraform/node/scripts/k8s-node.sh
+++ terraform/node/scripts/k8s-node.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# Replace this with the token
-TOKEN=9b60c2.a62d14dea50eebac
 
-MASTER_IP=165.227.78.8
+# Replace this with the token - use for testing and debug
+#TOKEN=9b60c2.a62d14dea50eebac
+
+#Uncomment to  use for testing and debug
+#MASTER_IP=165.227.78.8
 
 kubeadm join --token $TOKEN $MASTER_IP:6443
