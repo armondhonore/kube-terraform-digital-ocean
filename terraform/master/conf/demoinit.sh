b2c3d1eee453977eb6c94f65131305b4122b2ff1
armondhonore <armond.honore@gmail.com>
2017-08-14 11:12:42 -0400

    fix command error missing kubectl

--- terraform/master/conf/demoinit.sh
+++ terraform/master/conf/demoinit.sh
@@ -1,3 +1,3 @@
-create  -f db-pod.yml -f db-service.yml -f web-pod.yml -f web-service.yml
+kubectl create  -f db-pod.yml -f db-service.yml -f web-pod.yml -f web-service.yml
 sleep 30
 kubectl get all --all-namespaces
